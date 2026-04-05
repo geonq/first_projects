d68357a8af0b1233a180b5d099909b1a89be225d
Georg <domkegeorg05@gmail.com>
2026-04-05 13:46:04 +0200

    organise projects into folders

---
+++ black-scholes/pricer.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import numpy as np
+import scipy.stats as si
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import plotly.graph_objects as go
+from datetime import datetime
+
